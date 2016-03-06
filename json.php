8e2a3e82d4ac732f4eb7b2f86f98bd59f839bb3f
aurum <aurum.gallente@gmail.com>
2016-03-07 02:13:09 +0300

    removed duplicates from tags

--- json.php
+++ json.php
@@ -3,7 +3,7 @@ header("Content-type: application/json; charset=utf-8");
 require ('db.php');
 $query = "SELECT p.id as id, p.author as author, t.text as tag, mt.metatag as metatag, pg.title as title from posts as p
 INNER JOIN posts_tags AS pt ON (pt.post_id=p.id)
-INNER JOIN tags AS t ON (t.id=pt.tag_id)
+LEFT JOIN tags AS t ON (t.id=pt.tag_id)
 INNER JOIN pages_posts AS pp ON (pp.post_id=p.id)
 INNER JOIN pages AS pg ON (pg.id=pp.page_id)
 INNER JOIN metaTags AS mt ON (mt.page_id=pg.id)
@@ -21,7 +21,7 @@ foreach($results as $r){
   $posts[$r['id']]['tags'][]=$r['tag'];
   $posts[$r['id']]['title']=$r['title'];
   $posts[$r['id']]['metatag']=$r['metatag'];
+  $posts[$r['id']]['tags'] = array_unique($posts[$r['id']]['tags']);
   unset($posts['id']);
 }
 echo json_encode($posts, JSON_UNESCAPED_UNICODE);
-//file_put_contents('posts.json', json_encode($posts, JSON_UNESCAPED_UNICODE), FILE_APPEND);
